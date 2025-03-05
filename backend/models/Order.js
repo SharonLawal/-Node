ea577bdcfa861f2aa88ed95edcf62bad37bb5581
Sharon Lawal <138617365+SharonLawal@users.noreply.github.com>
2025-03-05 17:27:05 +0100

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -15,7 +15,6 @@ const orderSchema = new mongoose.Schema(
     shipping_address: { type: Object },
     billing_address: { type: Object },
     stripe_session_id: { type: String, required: false },
-    orderID: { type: String, default: () => new mongoose.Types.ObjectId().toString() },
   },
   { timestamps: true }
 );
